bded25920cbd05e959318ba52c6579b8e5395e68
Linghao Zhang <starrywhuzhang@gmail.com>
2024-11-20 19:24:25 +0800

    init instance

---
+++ Benchmark/Config.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using NewLife;
+
+namespace Benchmark
+{
+    internal class Config
+    {
+        #region 属性
+        public String Address { get; set; }
+
+        public Int32 Times { get; set; } = 10000;
+
+        /// <summary>并行度</summary>
+        public Int32 ConcurrentLevel { get; set; } = 100;
+
+        /// <summary>间隔，毫秒</summary>
+        public Int32 Interval { get; set; }
+
+        /// <summary>绑定的本地地址，*表示每一个分开绑定，支持输入一段 10.0.0.31-40，用于海量连接压测</summary>
+        public String Bind { get; set; }
+
+        public String Content { get; set; }
+
+        public Boolean Reply { get; set; }
+        #endregion
+
+        public void Parse(String[] args)
+        {
+            for (var i = 0; i < args.Length; i++)
+            {
+                switch (args[i].ToLower())
+                {
+                    case "-n":
+                        if (i + 1 < args.Length)
+                        {
+                            Times = args[i + 1].ToInt();
+                            i++;
+                        }
+                        break;
+                    case "-c":
+                        if (i + 1 < args.Length)
+                        {
+                            ConcurrentLevel = args[i + 1].ToInt();
+                            i++;
+                        }
+                        break;
+                    case "-i":
+                        if (i + 1 < args.Length)
+                        {
+                            Interval = args[i + 1].ToInt();
+                            i++;
+                        }
+                        break;
+                    case "-s":
+                        if (i + 1 < args.Length)
+                        {
+                            Content = args[i + 1];
+                            i++;
+                        }
+                        break;
+                    case "-b":
+                        if (i + 1 < args.Length)
+                        {
+                            Bind = args[i + 1];
+                            i++;
+                        }
+                        break;
+                    case "-r":
+                        Reply = true;
+                        break;
+                }
+            }
+
+            var str = args.LastOrDefault();
+            if (!str.StartsWith("-")) Address = str;
+        }
+
+        public IPAddress[] GetBinds()
+        {
+            if (Bind == "*") return NetHelper.GetIPsWithCache();
+
+            var addrs = new List<IPAddress>();
+            if (!Bind.IsNullOrEmpty())
+            {
+                // 支持输入一段 10.0.0.31-40
+                var p = Bind.IndexOf('-');
+                if (p > 0)
+                {
+                    var str = Bind.Substring(0, p);
+                    var p2 = str.LastIndexOfAny(new[] { '.', ':' });
+                    var prefix = str.Substring(0, p2 + 1);
+                    var ipv6 = prefix[^1] == ':';
+                    var start = str.Substring(p2 + 1).ToInt();
+                    var end = Bind.Substring(p + 1).ToInt();
+                    for (var i = start; i <= end; i++)
+                    {
+                        if (ipv6)
+                            addrs.Add(IPAddress.Parse(prefix + i.ToString("x")));
+                        else
+                            addrs.Add(IPAddress.Parse(prefix + i));
+                    }
+                }
+                else
+                {
+                    var binds = Bind.Split(",");
+                    foreach (var item in binds)
+                    {
+                        addrs.Add(IPAddress.Parse(item));
+                    }
+                }
+            }
+
+            return addrs.ToArray();
+        }
+    }
+}
---
+++ EchoAgent/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using NewLife;
+using NewLife.Agent;
+using NewLife.Log;
+using NewLife.Net;
+using NewLife.Threading;
+
+namespace EchoAgent
+{
+    class Program
+    {
+        static void Main(String[] args) => new MyService().Main(args);
+    }
+
+    class MyService : ServiceBase
+    {
+        public MyService()
+        {
+            ServiceName = "EchoAgent";
+            DisplayName = "回声服务";
+            Description = "这是NewLife.Net的一个回声服务示例！";
+        }
+
+        MyNetServer _Server;
+        /// <summary>开始服务</summary>
+        /// <param name="reason"></param>
+        public override void StartWork(String reason)
+        {
+            // 实例化服务端，指定端口，同时在Tcp/Udp/IPv4/IPv6上监听
+            var svr = new MyNetServer
+            {
+                Port = 1234,
+                Log = XTrace.Log,
+                Tracer = new DefaultTracer { Period = 15, Log = XTrace.Log },
+#if DEBUG
+                SocketLog = XTrace.Log,
+                LogSend = true,
+                LogReceive = true,
+#endif
+            };
+            svr.Start();
+
+            _Server = svr;
+
+            _timer1 = new TimerX(s => ShowStat(_Server), null, 1000, 1000) { Async = true };
+            _timer2 = new TimerX(s => SendTime(_Server), null, 1000, 1000) { Async = true };
+
+            base.StartWork(reason);
+        }
+
+        /// <summary>停止服务</summary>
+        /// <param name="reason"></param>
+        public override void StopWork(String reason)
+        {
+            _Server.TryDispose();
+            _Server = null;
+
+            base.StopWork(reason);
+        }
+
+        private TimerX _timer1;
+        private TimerX _timer2;
+
+        private String _last;
+        /// <summary>显示服务端状态</summary>
+        /// <param name="ns"></param>
+        private void ShowStat(NetServer ns)
+        {
+            if (ns == null) return;
+
+            var msg = ns.GetStat();
+            if (msg == _last) return;
+
+            _last = msg;
+
+            WriteLog(msg);
+        }
+
+        /// <summary>向所有客户端发送时间</summary>
+        /// <param name="ns"></param>
+        private void SendTime(NetServer ns)
+        {
+            if (ns == null) return;
+
+            var str = DateTime.Now.ToFullString() + Environment.NewLine;
+            var buf = str.GetBytes();
+            ns.SendAllAsync(buf);
+        }
+    }
+}
